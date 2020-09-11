70669ddc8583771cad42708f271c62c5cc0d3c5e
HiYanXL <2606469283@qq.com>
2020-09-11 17:12:34 +0800

    20200908

---
+++ learning/src/test/java/com/yxl/learning/usercases/TestSample.java
@@ -0,0 +1,12 @@
+package com.yxl.learning.usercases;
+
+import com.yxl.learning.LearningApplicationTests;
+import org.junit.jupiter.api.Test;
+
+public class TestSample extends LearningApplicationTests {
+    @Test
+    public void test() {
+
+
+    }
+}
